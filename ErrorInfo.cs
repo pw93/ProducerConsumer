cf1df7a846d33ae522c1da18c7904a0dc760e149
w93002 <w93002@gmail.com>
2025-07-23 11:17:27 +0800

    abc

--- ErrorInfo.cs
+++ ErrorInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 
-namespace test_uf_bot
+namespace ProfitWin.Common
 {
     public class ErrorInfo
     {
@@ -16,7 +16,7 @@ namespace test_uf_bot
             code = code1;
             msg1 = mg1;
             msg2 = mg2;
-        }
+        }       
 
         public void SetData(int code1, string mg1, string mg2 = "")
         {
@@ -27,15 +27,32 @@ namespace test_uf_bot
 
         public override string ToString()
         {
-            return $"ErrorInfo:{{{code}|{msg1}|{msg2}}}";
+            if (string.IsNullOrEmpty(msg1) && string.IsNullOrEmpty(msg2))
+                return $"ErrorInfo:{{{code}|_}}";
+            if (msg1 == msg2 || string.IsNullOrEmpty(msg2))
+                return $"ErrorInfo:{{{code}|{msg1}}}";
+            else
+                return $"ErrorInfo:{{{code}|{msg1}|{msg2}}}";
         }
 
 
         public static ErrorInfo Ok => new ErrorInfo();  // 快速取得正常情況
-        public static ErrorInfo QuickError(string msg1, string msg2 = "") =>
-        new ErrorInfo(-1, msg1, msg2);
+        public static ErrorInfo QuickError(string msg1, string msg2 = "")
+        {
+            if (string.IsNullOrEmpty(msg2))
+                return new ErrorInfo(-1, msg1, msg1);
+            else
+                return new ErrorInfo(-1, msg1, msg2);
+        }
+
+        public static ErrorInfo QuickError(string msg1, ErrorInfo sub)
+        {
+            return new ErrorInfo(-1, msg1, sub.msg2);
+        }        
 
         public static ErrorInfo QuickException(Exception ex) =>
-        new ErrorInfo(-1, "exception", ex.Message);
+        new ErrorInfo(-1, "exception", $"ex: {ex.Message}");
+
+        public bool IsOk => code == 0;
     }
 }
